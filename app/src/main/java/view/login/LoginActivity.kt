48ba03a700bdab10410ef3e5557f8d77548f1dda
Your Name <you@example.com>
2022-04-18 13:15:38 +0700

    add package view

--- app/src/main/java/view/login/LoginActivity.kt
+++ app/src/main/java/view/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.oxalis.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
